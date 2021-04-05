5120b86724a1b2013699bbe51a7b08cc5086d9a8
Vedat Can Keklik <vcankeklik@gmail.com>
2021-04-05 19:38:31 +0300

    [BugFix][MemLeak] Added unbind functions for proper cleanup of event listeners

--- src/directives/mousewheel.js
+++ src/directives/mousewheel.js
@@ -1,18 +1,28 @@
 import normalizeWheel from 'normalize-wheel';
 
 const isFirefox = typeof navigator !== 'undefined' && navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+const ctx = '@@mousewheelContext';
 
 const mousewheel = function(element, callback) {
   if (element && element.addEventListener) {
-    element.addEventListener(isFirefox ? 'DOMMouseScroll' : 'mousewheel', function(event) {
+    let bindingFn = function(event) {
       const normalized = normalizeWheel(event);
       callback && callback.apply(this, [event, normalized]);
-    });
+    };
+    element.addEventListener(isFirefox ? 'DOMMouseScroll' : 'mousewheel', bindingFn);
+    element[ctx] = { bindingFn };
   }
 };
 
 export default {
   bind(el, binding) {
     mousewheel(el, binding.value);
+  },
+  unbind(el) {
+    if (!el[ctx] || !el[ctx].bindingFn) {
+      return;
+    }
+    el.removeEventListener(isFirefox ? 'DOMMouseScroll' : 'mousewheel', el[ctx].bindingFn);
+    delete el[ctx];
   }
 };
